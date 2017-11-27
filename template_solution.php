d8fd41f1178374506c2802474199075debf408e3
崔楹彪 <244169731@qq.com>
2017-11-27 14:52:14 +0800

    ###

--- template_solution.php
+++ template_solution.php
@@ -6,16 +6,38 @@ Template Name:解决方案
 get_header(); ?>
   <div class="mo-solution">
     <h3>免费索取策划方案</h3>
-    <div class="recorde">最新申请<span><?php echo get_post_meta('1','最新人数',true); ?></span>人 累计已有<span><?php echo get_post_meta('1','累计人数',true); ?></span>位索取方案</div>
+    <div class="recorde">
+      <?php  global $wpdb; $query = "SELECT count(*) as count FROM ".$wpdb->prefix."comments where comment_post_ID = 1 and comment_approved = 1 and comment_date > '".date("Y-m-d H:i:s",strtotime("-4 week"))."';";
+          $firstrow = $wpdb->get_results($query);
+      ?>
+      最新申请<span><?php echo $firstrow['0']->count; ?></span>人 
+
+      <?php  global $wpdb; $query = "SELECT count(*) as count FROM ".$wpdb->prefix."comments where comment_post_ID = 1 and comment_approved = 1;";
+          $firstrow = $wpdb->get_results($query);
+      ?>
+      累计已有<span><?php echo $firstrow['0']->count; ?></span>位索取方案
+    </div>
     <div class="mo-solutionSlider">
       <ul>
-        <li>郭** 133****8787 5分钟前</li>
-        <li>王** 134****9082 13分钟前</li>
-        <li>李** 137****4988 36分钟前</li>
-        <li>张** 139****6721 40分钟前</li>
-        <li>黄** 136****9582 52分钟前</li>
-        <li>钱** 139****4781 1小时前</li>
-        <li>朱** 138****9313 1小时前</li>
+        <li>王** 132****8787 5分钟前</li>
+        <li>赵** 135****7982 6分钟前</li>
+        <li>李** 132****9082 13分钟前</li>
+        <li>汪** 136****4988 36分钟前</li>
+        <li>裘** 137****6721 40分钟前</li>
+        <li>宋** 134****9582 52分钟前</li>
+        <li>钱** 131****4781 1小时前</li>
+        <li>马** 139****9313 1小时前</li>
+        <li>叶** 133****4482 1小时前</li>
+        <li>申** 134****9121 2小时前</li>
+        <li>杜** 131****9303 2小时前</li>
+        <li>张** 137****9654 3小时前</li>
+        <li>水** 134****9582 3小时前</li>
+        <li>戴** 136****4781 3小时前</li>
+        <li>许** 131****9343 3小时前</li>
+        <li>徐** 131****4422 4小时前</li>
+        <li>崔** 133****9111 4小时前</li>
+        <li>孔** 134****9353 4小时前</li>
+        <li>张** 130****9657 4小时前</li>
       </ul>
     </div>
     <form method="post" action="#" onsubmit="return false;">
